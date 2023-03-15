8d57012732b62c888cec75b0a8c46f8a8fec3979
Armando Carrizoza <armandoxd_f.o.b@hotmail.com>
2023-03-14 18:53:03 -0700

    Componentes personajes, agregar e interfaz

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
 
 @Component({
   selector: 'app-main-page',
-  template: `
-  
-  <h1> {{ titulo }}</h1>
-  
-  `
+  templateUrl: `./main-page.component.html`
 })
 export class MainPageComponent {
 
-  titulo:string = 'DBZ';
+  personajes: Personaje[] = [
+  {
+    nombre: 'Goku',
+    poder: 15000
+  },
+  {
+    nombre: 'Vegeta',
+    poder: 8500
+  }
+];
+
+cambiarNombre( event: any){
+  console.log( event?.target.value );
+}
 
 }
